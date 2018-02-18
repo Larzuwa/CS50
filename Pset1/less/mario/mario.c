4e0f3d5c164166c939cbb779aab75b0bb78c62bc
Laura Vela Fernández <Laura@MacBook-Air-de-Laura.local>
2018-02-18 17:18:04 +0100

    pset1, mario and hello

---
+++ Pset1/less/mario/mario.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main (void) {
+
+//******** Variables ***********
+    int n; //Height
+
+
+//******** User's input ********
+    do {
+        printf("Enter height: ");
+        n=get_int();
+    }
+    while (n<0 || n>23);
+
+//******** Piramid *************
+    for (int i = 0; i < n; i++) {  //For each row
+        // Print spaces
+        for(int j = 0; j < n-i-1; j++)
+        {
+            printf(" ");
+        }
+        // Print hashes
+		for(int k = 0; k < i+2; k++)
+		{
+			printf("#");
+		}
+		printf("\n");
+	}
+	return 0;
+        // Print row line
+
+
+//******** END OF CODE *********
+}
