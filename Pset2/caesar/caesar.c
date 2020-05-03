ddf9763247bf1a887e561bb0c99d67c40d7ffc8c
Laura <larzuvva@gmail.com>
2020-05-03 23:19:19 +0200

    feat: convert string key to integer key

--- Pset2/caesar/caesar.c
+++ Pset2/caesar/caesar.c
@@ -2,9 +2,8 @@
 #include <cs50.h>
 #include <string.h>
 #include <math.h>
-#include <math.h>
 #include <ctype.h>
-
+#include <stdlib.h>
 
 
 char letters [52] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
@@ -12,32 +11,34 @@ char letters [52] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
                     };
 string key;
 string validkey;
-int main(int argc, string argv[])
+int keynumeric;
 
+int main(int argc, string argv[])
 {
     // Checks if the provided argument has two commands (the program + the key)
     // When the argument meets the requirements (just one argument)
     if (argc == 2)
     {
         key = argv[1];
+
         // We want to check that the key provided meets the requirements (is an integer)
         // For that we compare each character of the key against the numbers array
         for (int i = 0, n = strlen(key); i < n; i++)
         {
             if (isdigit(key[i]))
             {
-                validkey += key[i];
+                    validkey += key[i];
             }
-
             else
             {
                 printf("Usage: ./caesar key \n");
                 return 1;
             }
-
         }
+        // Convert the string to an integer
+        keynumeric = atoi(key);
         printf("Success\n");
-
+        printf("%i\n", keynumeric);
     }
 
     // If the program does not meet the requirements (no arguments or more than 2)
@@ -46,5 +47,4 @@ int main(int argc, string argv[])
         printf("Usage: ./caesar key \n");
         return 1;
     }
-
 }
